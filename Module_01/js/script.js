28d14243ef41a36b6e74ca4bd508f8d9912df93a
kosolapeg <kosolapeg@gmail.com>
2018-11-28 00:00:55 +0100

    Completed HW01

--- Module_01/js/script.js
+++ Module_01/js/script.js
@@ -1,2 +1,26 @@
 'use strict';
-console.log(Math.random(10));
+
+const adminLogin = 'admin';
+const adminPassword = 'm4ng0h4ckz';
+
+const messageLogin = 'Введите логин';
+const messagePsw = 'Введите пароль';
+const messageCancel = 'Отменено пользователем!';
+const messageRight = 'Добро пожаловать!';
+const messageWrong = 'Доступ запрещен, неверный пароль!';
+
+let login  = prompt (messageLogin);
+
+if (login === null) {
+  alert (messageCancel);
+} else if (login === adminLogin) {
+        let psw = prompt(messagePsw);
+        if (psw === null) {
+            alert (messageCancel)
+        } else if (psw === adminPassword) {
+            alert (messageRight);
+        } else {
+            alert (messageWrong);
+        }
+}    
+
