02d7e2df6f2473f07ea3ad81a433059c1ab44110
ArtVal <35500669+ArtVal1988@users.noreply.github.com>
2018-12-13 15:09:01 +0100

    Module_03 begin

---
+++ Module_03/js/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let userInput, userNumber;
+const numbers = [];
+let total = 0;
+
+do {
+    userInput = prompt('Введите новое число для массива');
+    if (userInput === null) break;
+    if (userInput !=='') {
+        userNumber = Number(userInput);
+        if (!Number.isNaN(userNumber)){
+            numbers.push(userNumber);
+        } else {
+            alert("Было введено не число, попробуйте еще раз");       
+        }
+    }
+    
+} while (true)
+
+
+if (numbers.length !== 0) {
+    for (let number of numbers) {
+        total +=number;
+    }
+    alert(`Общая сумма чисел равна ${total}`);
+}
